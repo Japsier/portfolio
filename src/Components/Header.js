d1d3534db57b637cc037fdd9d0f20ff0ef9a278a
JapSir <japsircode@gmail.com>
2023-01-13 16:46:47 +0100

    add header content

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,27 +14,16 @@ const Header = () => {
             </div>
             <div className="aboutText"> 
                 <img className="backupPicture" src={profile} alt="" />        
+                <h1 className="backupName">Name Lastname</h1>
                 <h2>About me</h2>
-                <p>Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. 
-                    Maecenas egestas posuere risus, 
-                    lobortis volutpat dolor pharetra nec. 
-                    Vivamus libero erat, 
-                    facilisis eget dignissim sit amet, 
-                    aliquet sit amet dolor. 
-                    Etiam convallis ultricies libero, 
-                    quis congue tortor faucibus id. 
-                    In blandit sit amet risus nec lacinia. 
-                    Nunc a metus purus. 
-                    Duis consequat non tortor ac ullamcorper. 
-                    Vestibulum ut erat non justo varius varius. 
-                    Pellentesque condimentum diam convallis tristique mollis. 
-                    Sed odio arcu, aliquam nec libero ultricies, 
-                    rhoncus commodo purus. 
-                    Curabitur erat eros, iaculis eu ullamcorper in, 
-                    iaculis vel ex. Aliquam eu arcu malesuada, accumsan dui et, 
-                    elementum sem. Ut ipsum neque, ullamcorper dictum luctus vitae, 
-                    sollicitudin eu velit. </p>
+                <p>Hello and welcome  to my website! 
+                    This website is my personal platform that showcases my skills and passion for web development. 
+                    As a student, I am dedicated to learning new things, 
+                    like new technologies and programming languages, and use that knowledge to build cool projects. 
+                    My website serves as a portfolio that showcases my work and my development journey. 
+                    The website is a reflection of my love for coding and my ambition to create meaningful and impactful projects. 
+                    The website also communicates that I am open to working with other ambitious people who share my passion for technology and development. 
+                    With my website, I aim to connect with like-minded individuals and explore opportunities to collaborate and grow together.</p>
             </div>
         </header>
     )
