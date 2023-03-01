201ad84ed18f60defe0501437a7ded2366034f93
JapSir <japsircode@gmail.com>
2023-03-01 13:07:44 +0100

    change project, add footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
     return(
         <footer>
             <ul className="contactLinks">
-                <li className="github"><img src={github} alt="github logo" /></li>
-                <li className="email" ><img src="https://cdn-icons-png.flaticon.com/128/2099/2099199.png" alt="github logo" /></li>
+                <li className="github"><a href="https://github.com/Japsier" target="_blank" rel="noreferrer"><img src={github} alt="github logo" /></a></li>
+                <li className="email" ><a href = "mailto:japsircode@gmail.com" target="_blank" rel="noreferrer"><img src="https://cdn-icons-png.flaticon.com/128/2099/2099199.png" alt="email logo" /></a></li>
             </ul>
             
         </footer>
